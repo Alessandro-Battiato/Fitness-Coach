bac176d26626bbf8103e2e267247740605a8ac48
alessandrobattiato <alessandroDev@outlook.it>
2025-04-13 17:08:36 +0200

    feat: project cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+  Nella hero, al posto della foto metti il modello. DEV'ESSERE GRABBABILE E ROTEABILE. In modo da evidenziare anche i dorsali glutei e polpacci per esercizi. Magari chiedi a GPT come aiutare l'utente a far capire che sta evidenziando zone cliccabili. Al click, dovrebbe spuntare una modale animata vedi tu, con la gif, e sotto descrizione istruzioni e alcune robe sull'esercizio (in mobile la modale come su CreationDose prende full screen)
+  Nella parte dove metti il modello 3d, devi TOGLIERE il quadrato "Choose your trainer" Ma valutare se LASCIARE
+  le diciture in basso sotto il modello come "100+ workout, satisfied" ecc e capire meglio come muoversi per la responsiveness chiaramente
+  E nei programs fai un carosello di alcune immagini molto semplicemente
+
+  Font family - Dark Grotesque
+
+  #94DA22
+  #FC6441
+  #FAB832
+  #101828
+  #F2F2F2
+  #475467
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return <></>;
 }
 
-export default App
+export default App;
